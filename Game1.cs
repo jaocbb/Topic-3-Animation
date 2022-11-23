d2df1c2f94126af865edc3cb2091bec07938af58
BattJaco773 <jacobb8800@gmail.com>
2022-11-23 09:24:22 -0500

    hi

--- Game1.cs
+++ Game1.cs
@@ -8,9 +8,9 @@ namespace Topic_3_Animation
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        Texture2D tribbleGreyTexture;
-        Rectangle tribbleGreyRect;
-        Vector2 tribbleGreySpeed;
+        Texture2D GreyTexture,BrownTexture,CreamTexture,OrangeTexture;
+        Rectangle GreyRect,BrownRect,CreamRect,OrangeRect;
+        Vector2 GreySpeed,BrownSpeed,CreamSpeed,OrangeSpeed;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -24,15 +24,24 @@ namespace Topic_3_Animation
             // TODO: Add your initialization logic here
             _graphics.PreferredBackBufferHeight = 500;
             _graphics.PreferredBackBufferWidth = 800;
-            tribbleGreyRect = new Rectangle(300, 10, 100,100);
-            tribbleGreySpeed = new Vector2(2,2); 
+            GreyRect = new Rectangle(300, 10, 100,100);
+            GreySpeed = new Vector2(0,3); 
+            BrownRect = new Rectangle(400, 250, 100, 100);
+            BrownSpeed = new Vector2(3, 0);
+            CreamRect = new Rectangle(700, 400, 100, 100);
+            CreamSpeed = new Vector2(3, 3);
+            OrangeRect = new Rectangle(0, 400, 100, 100);
+            OrangeSpeed = new Vector2(3, 3);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            tribbleGreyTexture = Content.Load<Texture2D>("TribbleGrey");
+            GreyTexture = Content.Load<Texture2D>("tribbleGrey");
+            BrownTexture = Content.Load<Texture2D>("tribbleBrown");
+            CreamTexture = Content.Load<Texture2D>("tribbleCream");
+            OrangeTexture = Content.Load<Texture2D>("tribbleOrange");
             // TODO: use this.Content to load your game content here
         }
 
@@ -42,14 +51,48 @@ namespace Topic_3_Animation
                 Exit();
 
             // TODO: Add your update logic here
-            tribbleGreyRect.X += (int)tribbleGreySpeed.X;
-            if (tribbleGreyRect.Right > _graphics.PreferredBackBufferHeight  || tribbleGreyRect.X < 0 )
-                tribbleGreySpeed.X *= -1;
+            GreyRect.X += (int)GreySpeed.X;
+            if (GreyRect.Right > _graphics.PreferredBackBufferWidth  || GreyRect.X < 0 )
+               GreySpeed.X *= -1;
             
-            tribbleGreyRect.Y += (int)tribbleGreySpeed.Y;
-            if (tribbleGreyRect.Bottom > _graphics.PreferredBackBufferHeight || tribbleGreyRect.Left<_graphics.PreferredBackBufferHeight)
-                
+            GreyRect.Y += (int)GreySpeed.Y;
+            if (GreyRect.Bottom > _graphics.PreferredBackBufferHeight ||GreyRect.Y<0)
+                GreySpeed.Y *= -1;
             base.Update(gameTime);
+
+            BrownRect.X += (int)BrownSpeed.X;
+            if (BrownRect.Right > _graphics.PreferredBackBufferWidth || BrownRect.X < 0)
+                BrownSpeed.X *= -1;
+
+            BrownRect.Y += (int)BrownSpeed.Y;
+            if (BrownRect.Bottom > _graphics.PreferredBackBufferHeight || BrownRect.Y < 0)
+                BrownSpeed.Y *= -1;
+            base.Update(gameTime);
+
+            CreamRect.X += (int)CreamSpeed.X;
+            if (CreamRect.Right > _graphics.PreferredBackBufferWidth || CreamRect.X < 0)
+                CreamSpeed.X *= -1;
+
+            CreamRect.Y += (int)CreamSpeed.Y;
+            if (CreamRect.Bottom > _graphics.PreferredBackBufferHeight || CreamRect.Y < 0)
+                CreamSpeed.Y *= -1;
+            base.Update(gameTime);
+
+
+            OrangeRect.X += (int)OrangeSpeed.X;
+            if (OrangeRect.Right > _graphics.PreferredBackBufferWidth || OrangeRect.X < 0)
+                OrangeSpeed.X *= -1;
+
+
+            OrangeRect.Y += (int)OrangeSpeed.Y;
+            if (OrangeRect.Bottom > _graphics.PreferredBackBufferHeight || OrangeRect.Y < 0)
+                OrangeSpeed.Y *= -1;
+            base.Update(gameTime);
+
+
+
+
+
         }
 
         protected override void Draw(GameTime gameTime)
@@ -58,8 +101,10 @@ namespace Topic_3_Animation
             GraphicsDevice.Clear(Color.Black);
             _spriteBatch.Begin();
 
-            _spriteBatch.Draw(tribbleGreyTexture, tribbleGreyRect, Color.White);
-
+            _spriteBatch.Draw(GreyTexture, GreyRect, Color.White);
+            _spriteBatch.Draw(BrownTexture, BrownRect, Color.White);
+            _spriteBatch.Draw(CreamTexture, CreamRect, Color.White);
+            _spriteBatch.Draw(OrangeTexture, OrangeRect, Color.White);
             _spriteBatch.End();
             // TODO: Add your drawing code here
 
