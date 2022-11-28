869455a0ebbe51f6d2618a82199a933fd118aa72
BattJaco773 <jacobb8800@gmail.com>
2022-11-28 09:37:30 -0500

    Need sound

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -11,6 +12,7 @@ namespace Topic_3_Animation
         Texture2D GreyTexture,BrownTexture,CreamTexture,OrangeTexture;
         Rectangle GreyRect,BrownRect,CreamRect,OrangeRect;
         Vector2 GreySpeed,BrownSpeed,CreamSpeed,OrangeSpeed;
+        SoundEffect tribbleSound;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -42,6 +44,7 @@ namespace Topic_3_Animation
             BrownTexture = Content.Load<Texture2D>("tribbleBrown");
             CreamTexture = Content.Load<Texture2D>("tribbleCream");
             OrangeTexture = Content.Load<Texture2D>("tribbleOrange");
+            tribbleSound = Content.Load<SoundEffect>("tribbleSound");
             // TODO: use this.Content to load your game content here
         }
 
@@ -52,12 +55,19 @@ namespace Topic_3_Animation
 
             // TODO: Add your update logic here
             GreyRect.X += (int)GreySpeed.X;
-            if (GreyRect.Right > _graphics.PreferredBackBufferWidth  || GreyRect.X < 0 )
-               GreySpeed.X *= -1;
-            
+            if (GreyRect.Right > _graphics.PreferredBackBufferWidth || GreyRect.X < 0)
+            {
+                GreySpeed.X *= -1;
+                tribbleSound.Play();
+            }
+
             GreyRect.Y += (int)GreySpeed.Y;
-            if (GreyRect.Bottom > _graphics.PreferredBackBufferHeight ||GreyRect.Y<0)
+            if (GreyRect.Bottom > _graphics.PreferredBackBufferHeight || GreyRect.Y < 0)
+            {
                 GreySpeed.Y *= -1;
+                tribbleSound.Play();
+            }
+                
             base.Update(gameTime);
 
             BrownRect.X += (int)BrownSpeed.X;
@@ -66,29 +76,49 @@ namespace Topic_3_Animation
 
             BrownRect.Y += (int)BrownSpeed.Y;
             if (BrownRect.Bottom > _graphics.PreferredBackBufferHeight || BrownRect.Y < 0)
+            {
                 BrownSpeed.Y *= -1;
+                tribbleSound.Play();
+            }
+                
             base.Update(gameTime);
 
             CreamRect.X += (int)CreamSpeed.X;
             if (CreamRect.Right > _graphics.PreferredBackBufferWidth || CreamRect.X < 0)
+            {
                 CreamSpeed.X *= -1;
+                tribbleSound.Play();
+            }
+               
 
             CreamRect.Y += (int)CreamSpeed.Y;
             if (CreamRect.Bottom > _graphics.PreferredBackBufferHeight || CreamRect.Y < 0)
+            {
                 CreamSpeed.Y *= -1;
+                tribbleSound.Play();
+            }
+               
             base.Update(gameTime);
 
 
             OrangeRect.X += (int)OrangeSpeed.X;
             if (OrangeRect.Right > _graphics.PreferredBackBufferWidth || OrangeRect.X < 0)
+            {
                 OrangeSpeed.X *= -1;
+                tribbleSound.Play();
+            }
+               
 
 
             OrangeRect.Y += (int)OrangeSpeed.Y;
             if (OrangeRect.Bottom > _graphics.PreferredBackBufferHeight || OrangeRect.Y < 0)
+            {
                 OrangeSpeed.Y *= -1;
+                tribbleSound.Play();
+            }
+                
             base.Update(gameTime);
-
+           
 
 
 
@@ -106,7 +136,7 @@ namespace Topic_3_Animation
             _spriteBatch.Draw(CreamTexture, CreamRect, Color.White);
             _spriteBatch.Draw(OrangeTexture, OrangeRect, Color.White);
             _spriteBatch.End();
-            // TODO: Add your drawing code here
+            // TODO: Add your drawing code  here
 
             base.Draw(gameTime);
         }
