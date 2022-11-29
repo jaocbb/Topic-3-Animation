430edbe230f391e9a55731844cd709345e116eaa
BattJaco773 <jacobb8800@gmail.com>
2022-11-29 09:32:44 -0500

    Done

--- Game1.cs
+++ Game1.cs
@@ -12,7 +12,22 @@ namespace Topic_3_Animation
         Texture2D GreyTexture,BrownTexture,CreamTexture,OrangeTexture;
         Rectangle GreyRect,BrownRect,CreamRect,OrangeRect;
         Vector2 GreySpeed,BrownSpeed,CreamSpeed,OrangeSpeed;
+        
         SoundEffect tribbleSound;
+        
+        Texture2D introTexture;
+        Rectangle introRect;
+        SpriteFont font;
+        Screen screen;
+
+        MouseState mouseState;
+        KeyboardState keyboardState;
+        enum Screen 
+        {
+            Intro,
+            TribbleYard,
+        }
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -24,6 +39,8 @@ namespace Topic_3_Animation
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            screen = Screen.Intro;
+
             _graphics.PreferredBackBufferHeight = 500;
             _graphics.PreferredBackBufferWidth = 800;
             GreyRect = new Rectangle(300, 10, 100,100);
@@ -34,6 +51,10 @@ namespace Topic_3_Animation
             CreamSpeed = new Vector2(3, 3);
             OrangeRect = new Rectangle(0, 400, 100, 100);
             OrangeSpeed = new Vector2(3, 3);
+            introRect = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
+            
+            
+            
             base.Initialize();
         }
 
@@ -45,84 +66,94 @@ namespace Topic_3_Animation
             CreamTexture = Content.Load<Texture2D>("tribbleCream");
             OrangeTexture = Content.Load<Texture2D>("tribbleOrange");
             tribbleSound = Content.Load<SoundEffect>("tribbleSound");
+            introTexture = Content.Load<Texture2D>("introScreen");
+            font = Content.Load<SpriteFont>("font");
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
+            mouseState = Mouse.GetState();
+            keyboardState = Keyboard.GetState();
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
-            GreyRect.X += (int)GreySpeed.X;
-            if (GreyRect.Right > _graphics.PreferredBackBufferWidth || GreyRect.X < 0)
+
+            if (screen == Screen.Intro)
             {
-                GreySpeed.X *= -1;
-                tribbleSound.Play();
+                if (mouseState.LeftButton == ButtonState.Pressed)
+                    screen = Screen.TribbleYard;
             }
-
-            GreyRect.Y += (int)GreySpeed.Y;
-            if (GreyRect.Bottom > _graphics.PreferredBackBufferHeight || GreyRect.Y < 0)
+            else if (screen == Screen.TribbleYard)
             {
-                GreySpeed.Y *= -1;
-                tribbleSound.Play();
+                GreyRect.X += (int)GreySpeed.X;
+                if (GreyRect.Right > _graphics.PreferredBackBufferWidth || GreyRect.X < 0)
+                {
+                    GreySpeed.X *= -1;
+                    tribbleSound.Play();
+                }
+
+                GreyRect.Y += (int)GreySpeed.Y;
+                if (GreyRect.Bottom > _graphics.PreferredBackBufferHeight || GreyRect.Y < 0)
+                {
+                    GreySpeed.Y *= -1;
+                    tribbleSound.Play();
+                }
+
+                base.Update(gameTime);
+
+                BrownRect.X += (int)BrownSpeed.X;
+                if (BrownRect.Right > _graphics.PreferredBackBufferWidth || BrownRect.X < 0)
+                    BrownSpeed.X *= -1;
+
+                BrownRect.Y += (int)BrownSpeed.Y;
+                if (BrownRect.Bottom > _graphics.PreferredBackBufferHeight || BrownRect.Y < 0)
+                {
+                    BrownSpeed.Y *= -1;
+                    tribbleSound.Play();
+                }
+
+                base.Update(gameTime);
+
+                CreamRect.X += (int)CreamSpeed.X;
+                if (CreamRect.Right > _graphics.PreferredBackBufferWidth || CreamRect.X < 0)
+                {
+                    CreamSpeed.X *= -1;
+                    tribbleSound.Play();
+                }
+
+
+                CreamRect.Y += (int)CreamSpeed.Y;
+                if (CreamRect.Bottom > _graphics.PreferredBackBufferHeight || CreamRect.Y < 0)
+                {
+                    CreamSpeed.Y *= -1;
+                    tribbleSound.Play();
+                }
+
+                base.Update(gameTime);
+
+
+                OrangeRect.X += (int)OrangeSpeed.X;
+                if (OrangeRect.Right > _graphics.PreferredBackBufferWidth || OrangeRect.X < 0)
+                {
+                    OrangeSpeed.X *= -1;
+                    tribbleSound.Play();
+                }
+
+
+
+                OrangeRect.Y += (int)OrangeSpeed.Y;
+                if (OrangeRect.Bottom > _graphics.PreferredBackBufferHeight || OrangeRect.Y < 0)
+                {
+                    OrangeSpeed.Y *= -1;
+                    tribbleSound.Play();
+                }
+
+                base.Update(gameTime);
+
             }
-                
-            base.Update(gameTime);
-
-            BrownRect.X += (int)BrownSpeed.X;
-            if (BrownRect.Right > _graphics.PreferredBackBufferWidth || BrownRect.X < 0)
-                BrownSpeed.X *= -1;
-
-            BrownRect.Y += (int)BrownSpeed.Y;
-            if (BrownRect.Bottom > _graphics.PreferredBackBufferHeight || BrownRect.Y < 0)
-            {
-                BrownSpeed.Y *= -1;
-                tribbleSound.Play();
-            }
-                
-            base.Update(gameTime);
-
-            CreamRect.X += (int)CreamSpeed.X;
-            if (CreamRect.Right > _graphics.PreferredBackBufferWidth || CreamRect.X < 0)
-            {
-                CreamSpeed.X *= -1;
-                tribbleSound.Play();
-            }
-               
-
-            CreamRect.Y += (int)CreamSpeed.Y;
-            if (CreamRect.Bottom > _graphics.PreferredBackBufferHeight || CreamRect.Y < 0)
-            {
-                CreamSpeed.Y *= -1;
-                tribbleSound.Play();
-            }
-               
-            base.Update(gameTime);
-
-
-            OrangeRect.X += (int)OrangeSpeed.X;
-            if (OrangeRect.Right > _graphics.PreferredBackBufferWidth || OrangeRect.X < 0)
-            {
-                OrangeSpeed.X *= -1;
-                tribbleSound.Play();
-            }
-               
-
-
-            OrangeRect.Y += (int)OrangeSpeed.Y;
-            if (OrangeRect.Bottom > _graphics.PreferredBackBufferHeight || OrangeRect.Y < 0)
-            {
-                OrangeSpeed.Y *= -1;
-                tribbleSound.Play();
-            }
-                
-            base.Update(gameTime);
-           
-
-
-
-
         }
 
         protected override void Draw(GameTime gameTime)
@@ -130,15 +161,29 @@ namespace Topic_3_Animation
             
             GraphicsDevice.Clear(Color.Black);
             _spriteBatch.Begin();
-
-            _spriteBatch.Draw(GreyTexture, GreyRect, Color.White);
-            _spriteBatch.Draw(BrownTexture, BrownRect, Color.White);
-            _spriteBatch.Draw(CreamTexture, CreamRect, Color.White);
-            _spriteBatch.Draw(OrangeTexture, OrangeRect, Color.White);
+            if(screen == Screen.Intro) 
+            {
+                _spriteBatch.Draw(introTexture, introRect, Color.White);
+                _spriteBatch.DrawString(font, "Click your left mouse button to start and right button to quit", new Vector2(250, 150), Color.White);
+            }
+            else if(screen == Screen.TribbleYard)
+            {
+                _spriteBatch.Draw(GreyTexture, GreyRect, Color.White);
+                _spriteBatch.Draw(BrownTexture, BrownRect, Color.White);
+                _spriteBatch.Draw(CreamTexture, CreamRect, Color.White);
+                _spriteBatch.Draw(OrangeTexture, OrangeRect, Color.White);
+               
+            }
+            if (mouseState.RightButton == ButtonState.Pressed)
+            {
+                Exit();
+            }
             _spriteBatch.End();
             // TODO: Add your drawing code  here
 
             base.Draw(gameTime);
         }
+       
     }
+
 }
